2098ffbc9d69f6018484b3d0a360d6edc6fb87b5
Dmitry0402 <dmitry.lukash@fortgroup.ae>
2026-07-13 11:51:51 +0300

    Improve Zoho token logging

--- services/zoho.js
+++ services/zoho.js
@@ -2,22 +2,34 @@ const axios = require("axios");
 const qs = require("qs");
 
 async function getAccessToken() {
-    const response = await axios.post(
-        "https://accounts.zoho.com/oauth/v2/token",
-        qs.stringify({
-            refresh_token: process.env.ZOHO_REFRESH_TOKEN,
-            client_id: process.env.ZOHO_CLIENT_ID,
-            client_secret: process.env.ZOHO_CLIENT_SECRET,
-            grant_type: "refresh_token"
-        }),
-        {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
+    try {
+        const response = await axios.post(
+            "https://accounts.zoho.com/oauth/v2/token",
+            qs.stringify({
+                refresh_token: process.env.ZOHO_REFRESH_TOKEN,
+                client_id: process.env.ZOHO_CLIENT_ID,
+                client_secret: process.env.ZOHO_CLIENT_SECRET,
+                grant_type: "refresh_token"
+            }),
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
             }
-        }
-    );
+        );
 
-    return response.data.access_token;
+        console.log("Zoho token received successfully");
+
+        return response.data.access_token;
+
+    } catch (error) {
+
+        console.error("ZOHO TOKEN ERROR");
+        console.error(error.response?.status);
+        console.error(error.response?.data);
+
+        throw error;
+    }
 }
 
 module.exports = {
